0c89b6efaaea32754252c0bf6334b314532fe1fc
dilipgupta123-123 <GDIlip10#$%>
2022-09-22 23:37:23 +0530

    first commit

---
+++ ReverseSdk/src/main/java/com/example/reversesdk/SdkActivity.kt
@@ -0,0 +1,12 @@
+package com.example.reversesdk
+
+import android.content.Context
+import android.widget.Toast
+
+class SdkActivity {
+
+    fun ShowToast(context:Context)
+    {
+        Toast.makeText(context,"Done Data",Toast.LENGTH_LONG).show()
+    }
+}
